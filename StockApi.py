0480257fa93cf9301d306ca8927b11e98785a848
nihalsandadi <sandadi.ni@northeastern.edu>
2025-03-15 16:21:10 -0700

    api for latest stock price

--- StockApi.py
+++ StockApi.py
@@ -4,13 +4,13 @@ import json
 
 
 def get_stock_data(ticker):
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + {ticker} + "&interval=5min&outputsize=compact&apikey=M0N0LCYG3J1OHGQY"
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + ticker + "&interval=5min&outputsize=compact&apikey=M0N0LCYG3J1OHGQY"
     response = requests.get(url)
 
     # Check if the response is successful
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        # print(data)
         # last_refreshed = data["Meta Data"]["3. Last Refreshed"]
         # price = data["Time Series (5min)"][last_refreshed]["1. open"]
         with open('data.json', 'w') as file:
@@ -19,14 +19,21 @@ def get_stock_data(ticker):
     else:
         return None
 
+def get_latest_price(data):
+    close_value = 0
+
+    if data is not None:
+        first_element_key = list(data["Time Series (5min)"].keys())[0]
+        close_value = data["Time Series (5min)"][first_element_key]["4. close"]
+    return close_value
+
 
 # stock_prices = {}
 symbol = "SPY"
-latest_price = "Time Series (5min)"
 data = get_stock_data(symbol)
 
-if data is not None:
-    stock_prices = data[latest_price]
+
+close_value = get_latest_price(data)
 
 
-print(f"{symbol}: {data}")
+print(f"{symbol}: {close_value}")
